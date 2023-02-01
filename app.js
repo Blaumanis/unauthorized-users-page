daf4c42f07bfa2a9de4ec0bc24aa216227309aeb
Blaumanis <blaumanis.alberts@gmail.com>
2023-02-01 14:22:34 +0200

    added styles

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
@@ -6,54 +6,48 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-const port = 2000;
+const port = 2000
 
-let data;
-app.post("/api/data", (req, res) => {
-    data = req.body
-    console.log(data)
-    res.send({ success: true });
-});
-
-app.get('/data', (req,res) => {
-  res.json(data)
+let data
+app.post('/api/data', (req, res) => {
+  data = req.body
+  console.log(data)
+  res.send({ success: true })
 })
 
-
-// app.get("/", (req, res) => res.type('html').send(html));
-
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   let html = `
     <!DOCTYPE html>
     <html>
       <head>
         <title>Hello from Render!</title>
-        ...
+        <style>
+          body{
+              display:flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+            }
+          
+          .card {
+  
+          }
+        </style>
       </head>
       <body>
-        <section>
-          Hello from Render!! ${data ? data?.name : 'hello'}
-        </section>
+        <div class="card">
+          <img width='300' src=${data?.backgroundImage} alt="card-img"/>
+          <h3>Name: ${data?.name}</h3>
+          <h3>Position: ${data?.position}</h3>
+          <h3>Phone: ${data?.phone}</h3>
+          <h3>Email: ${data?.email}</h3>
+          <h3>Website: ${data?.website}</h3>
+          <h3>Address: ${data?.address}</h3>
+        </div>
       </body>
     </html>
   `
-  res.type('html').send(html);
-});
+  res.type('html').send(html)
+})
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-// const html = `
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Hello from Render!</title>
-//     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-//     <script>
-//     </script>
-//   </head>
-//   <body>
-//   </body>
-// </html>
-// `
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
